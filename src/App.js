b01d8adb161f1cad17b5b453bf0c1df989dcdfdb
cristian_a_l <lutacristian@gmail.com>
2023-02-23 00:21:39 +0100

    footer v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-    <h1 style={{color:'Blue'}}>Welcome to Movie App Project</h1>    
+    <h1 style={{color:'Blue'}}>Welcome to Movie App Project</h1>   
+    <Footer /> 
     </div>
   );
 }
