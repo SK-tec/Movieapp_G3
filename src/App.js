f27cabb22e6ff722515313654e6ae5501bc79173
cristian_a_l <lutacristian@gmail.com>
2023-02-23 01:54:18 +0100

    nav_v1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+    <Header />
     <h1 style={{color:'Blue'}}>Welcome to Movie App Project</h1>   
     <Footer /> 
     </div>
