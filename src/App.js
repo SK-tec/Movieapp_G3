db679b4d26111cfceaeb1502fa8d3995fe661117
SK-tec <samathagamidi@gmail.com>
2023-02-24 08:55:05 +0100

    formadded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import NewMovie from './components/NewMovie';
 
 function App() {
   return (
     <div className="App">
-    <h1 style={{color:'Blue'}}>Welcome to Movie App Project</h1>    
+    <h1 style={{color:'Blue'}}>Welcome to Movie App Project</h1>  
+    <NewMovie/>  
     </div>
   );
 }
--- src/components/NewMovie.js
+++ src/components/NewMovie.js
@@ -1,6 +1,61 @@
-
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 const NewMovie = () => {
     return ( <>
+    
+     <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Title</Form.Label>
+          <Form.Control  placeholder="Enter Movie Title" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Director</Form.Label>
+          <Form.Control  placeholder="Enter Director Name" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
     
     </> );
 }
