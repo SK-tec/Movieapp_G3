28a48ff79d353d2785217f69f3fe259ab63a2fb1
unknown <savithakrishnan2021@gmail.com>
2023-02-26 20:47:24 +0100

    pushing into my branch

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Hero/>
     <h1 style={{color:'Blue'}}>Welcome to Movie App Project</h1>   
-    <Footer /> 
+    <Footer />  
     </div>
   );
 }
