899b21bb8feaa3a1fbbad431cc4f692ab2325ad7
SK-tec <samathagamidi@gmail.com>
2023-03-02 11:24:17 +0100

    .env file,New Movie component and List of Movies added

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,54 +1,44 @@
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
 const Movies = () => {
+const [movies,setMovies]=useState([]);
+useEffect(()=>{
+  axios
+  .get(`${process.env.REACT_APP_SERVER_BASE_URL}/api/movies`)
+  .then((res)=>{
+    console.log(res.data.url)
+    setMovies(res.data)})
+  .catch((e)=>console.log(e))
+},[])
     return ( <>
-<Container className='mt-5 '>
-<CardGroup >
-      <Card>
-        <Card.Img variant="top" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTmk3Vi727WknMpSA3d0QawqyJD0XrQdBQT0BzjzMnPXiRBQQXz" />
+    <Container className='mt-5 p-4'>
+      <Row>
+        
+        {movies.map((movie)=>(
+        
+        <Card style={{ width: '25rem'}} className="m-2" >
+        <Card.Img variant="top" src={movie.poster}/>
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title>{movie.title}</Card.Title>
           <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            {movie.synopsis}
           </Card.Text>
         </Card.Body>
         <Card.Footer>
-          <small className="text-muted">Rating:</small>
+          <small className="text-muted">Rating:{movie.rating}</small>
         </Card.Footer>
       </Card>
-      <Card>
-        <Card.Img variant="top" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTqgWkU4lJ49fP-biZlp_kNAWbfINefWmuBHNrMpTP_KCBMqvyw" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Rating:</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTjYlk1fycL4rS4fsfxbWMVW9GgUds95lxG_CrhypJZvBJx-6i" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text className='h-25'>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Rating:</small>
-        </Card.Footer>
-      </Card>
-    </CardGroup>
-</Container>
-    
-    
+      ))}
+        
+      </Row>
+      </Container>
+   
     </> );
 }
  
--- src/components/NewMovie.js
+++ src/components/NewMovie.js
@@ -1,87 +1,161 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-import { Container } from 'react-bootstrap';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import { Container } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const NewMovie = () => {
-  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    title: "",
+    director: "",
+    year: 0,
+    rating: 0,
+    poster: "",
+    synopsis: "",
+  });
+  const resetButton = (e) => {
+    setForm({
+      title: "",
+      director: "",
+      year: 0,
+      rating: 0,
+      poster: "",
+      synopsis: "",
+    });
+  };
 
+  const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-    return ( <>
-    <Container className='mt-5'>
-    <Button variant="primary" className='mb-5' onClick={handleShow}>
-        Add New Movie
-      </Button>
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>New Movie</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-            required
-            type="text"
-            placeholder="Movie Title"            
-          />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Director</Form.Label>
-              <Form.Control
-            required
-            type="text"
-            placeholder="Director Name"            
-          />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Year</Form.Label>
-              <Form.Control
-            required
-            type="text"
-            placeholder="Release Year"            
-          />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Rating</Form.Label>
-              <Form.Control
-            required
-            type="text"
-            placeholder="Movie Rating"            
-          />           
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Poster</Form.Label>
-              <Form.Control
-            required
-            type="text"
-            placeholder="Enter Image URL"            
-          />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Synopsis</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Add
-          </Button>
-        </Modal.Footer>
-      </Modal>
+  const handleSave = (e) => {
+    e.preventDefault();
+    console.log(`${process.env.REACT_APP_SERVER_BASE_URL}/api/movies`);
+    console.log(form);
+    axios
+      .post(`${process.env.REACT_APP_SERVER_BASE_URL}/api/movies`, form)
+      .then((res) => {
+        navigate("/");
+        resetButton();
+      })
+      .catch((e) => console.log(e));
+  };
+  return (
+    <>
+      <Container className="mt-5">
+        <Button variant="primary" className="mb-5" onClick={handleShow}>
+          Add New Movie
+        </Button>
 
-    
-    </Container>
-    </> );
-}
- 
+        <Modal show={show} onHide={handleClose} className="my-modal">
+          <Modal.Header closeButton>
+            <Modal.Title>New Movie</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  name="title"
+                  value={form.title}
+                  onChange={handleInput}
+                  placeholder="Movie Title"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Director</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  name="director"
+                  value={form.director}
+                  onChange={handleInput}
+                  placeholder="Director Name"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Year</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  name="year"
+                  value={form.year}
+                  onChange={handleInput}
+                  placeholder="Release Year"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Rating</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  name="rating"
+                  value={form.rating}
+                  onChange={handleInput}
+                  placeholder="Movie Rating"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Poster</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  name="poster"
+                  value={form.poster}
+                  onChange={handleInput}
+                  placeholder="Enter Image URL"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Synopsis</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  name="synopsis"
+                  value={form.synopsis}
+                  onChange={handleInput}
+                  rows={3}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleSave}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
+    </>
+  );
+};
+
 export default NewMovie;
