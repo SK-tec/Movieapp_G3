49c726a8da862032fdae744e386ff340afd84a74
SK-tec <67866435+SK-tec@users.noreply.github.com>
2023-03-03 14:33:37 +0100

    Revert "Hero section v2"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import NewMovie from "./NewMovie";
-//import Hero from "./Hero";
+import Hero from "./Hero";
 import Movies from "./Movies";
-import Hero2 from "./Hero2";
  
 const Header = () => {
     return ( <>
-    <Hero2/>
-    
+    <Hero/>
     <Movies/>
     <NewMovie/>
     </> );
